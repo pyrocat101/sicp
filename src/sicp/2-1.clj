56500d45c25bf5e4be5c0f2c16b1de61471d96ee
pyrocat <i@pyroc.at>
2013-07-13 00:54:38 +0800

    to 2.38

--- src/sicp/2-1.clj
+++ src/sicp/2-1.clj
@@ -352,19 +352,87 @@
 
 ; Exercise 2.33
 
+(defn accumulate [op initial sequence]
+  (if (empty? sequence)
+    initial
+    (op (first sequence)
+        (accumulate op initial (rest sequence)))))
+
+(defn map' [p sequence]
+  (accumulate (fn [x y] (cons (p x) y)) '() sequence))
+(defn append' [seq1 seq2]
+  (accumulate cons seq2 seq1))
+(defn length' [sequence]
+  (accumulate (fn [x y] (inc y)) 0 sequence))
+
+(map' square '(1 2 3 4 5))
+(append' '(1 2) '(3 4))
+(length' '(1 2 3 4 5))
 
 
+; Exercise 2.34
+
+(defn horner-eval [x coefficient-sequence]
+  (accumulate (fn [this-coeff higher-terms]
+                (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
 
 
+; Exercise 2.35
+
+(defn count-leaves [t]
+  (accumulate +
+              0
+              (map (fn [tree]
+                     (if (seq? tree)
+                       (count-leaves tree)
+                       1))
+                   t)))
+(count-leaves '((1 2) (3 4)))
 
 
+; Exercise 2.36
+
+(defn accumulate-n [op init seqs]
+  (if (= (first seqs) '())
+    '()
+    (cons (accumulate op init (map first seqs))
+          (accumulate-n op init (map rest seqs)))))
+(accumulate-n + 0 '((1  2  3)
+                    (4  5  6)
+                    (7  8  9)
+                    (10 11 12)))
 
 
+; Exercise 2.37
+
+(defn dot-product [v w]
+  (accumulate + 0 (map * v w)))
+
+(defn matrix-*-vector [m v]
+  (map #(dot-product % v) m))
+
+(defn transpose [mat]
+  (accumulate-n cons '() mat))
+
+(defn matrix-*-matrix [m n]
+  (let [cols (transpose n)]
+    (map #(matrix-*-vector cols %) m)))
+
+(def m '((1 2 3 4)
+         (4 5 6 6)
+         (6 7 8 9)))
+
+(matrix-*-vector m '(1 2 3 4))
+(transpose m)
+(matrix-*-matrix m (transpose m))
 
 
+; Exercise 2.38
 
-
-
-
+; `op`'s operands must be commutative
 
 
